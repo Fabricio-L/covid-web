bf0aae3e94467d833b8b7d7232de526ec30bace0
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-27 20:35:52 -0300

    Define theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import './App.css'
+import React, { Fragment } from 'react'
+import { ThemeProvider } from '@mui/material/styles'
+import { Box } from '@mui/material'
+import CssBaseline from '@mui/material/CssBaseline'
+import darkTheme from './themeConfig'
 
-function App() {
-  return <div className="App">Hola</div>
+const App = () => {
+  return (
+    <Fragment>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Box height={'100vh'}>Covid App</Box>
+      </ThemeProvider>
+    </Fragment>
+  )
 }
 
 export default App
