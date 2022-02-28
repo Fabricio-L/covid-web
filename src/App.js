f6e865ab886042902e525a8e568c8b0e5d1a133f
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-02-27 21:56:29 -0300

    Define structure

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { ThemeProvider } from '@mui/material/styles'
 import { Box } from '@mui/material'
 import CssBaseline from '@mui/material/CssBaseline'
 import darkTheme from './themeConfig'
+import Home from './views/Home'
 
 const App = () => {
   return (
     <Fragment>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-        <Box height={'100vh'}>Covid App</Box>
+        <Box height={'100vh'}>
+          <Home />
+        </Box>
       </ThemeProvider>
     </Fragment>
   )
---
+++ src/utils/apiGet.js
@@ -0,0 +1,3 @@
+const apiGet = () => {}
+
+export default apiGet
